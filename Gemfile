fb1730d6a48416fd1cd5c99ebc1ac6a8338b5948
Bryant Hughes <bryant@thegoodlab.com>
2013-11-13 16:24:56 -0600

    adding grunt file

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "https://rubygems.org"
+# Replace 4.0.3 with the version of Foundation you want to use
+gem "zurb-foundation", "4.2.2"
+# gem "compass"
+
+gem 'sass', '3.3.0.alpha.149'
+gem 'compass', '0.12.2'
+gem 'compass-sourcemaps', "~> 0.12.2.sourcemaps.57a186c"
+gem 'capistrano', "2.15.4"
+gem "railsless-deploy"
--- Gruntfile.js
+++ Gruntfile.js
@@ -46,14 +46,14 @@ module.exports = function(grunt) {
         files: [
 
           // Foundation
-          {cwd: "bower_modules/foundation/js", src: '**', dest: 'public/assets/scripts/vendor', expand: true, flatten: false},
-          {cwd: "bower_modules/foundation/scss/foundation", src: '**', dest: 'public/assets/styles/sass/foundation', expand: true, flatten: false},
-          {isFile: true, rename: function(dest, src){ return dest + "_" + src; }, cwd: "bower_modules/foundation/scss", src: 'foundation.scss', dest: 'public/assets/styles/sass/', expand: true, flatten: false},
-          {isFile: true, rename: function(dest, src){ return dest + "_" + src; }, cwd: "bower_modules/foundation/scss", src: 'normalize.scss', dest: 'public/assets/styles/sass/', expand: true, flatten: false},
+          {cwd: "bower_components/foundation/js", src: '**', dest: 'public/assets/scripts/vendor', expand: true, flatten: false},
+          {cwd: "bower_components/foundation/scss/foundation", src: '**', dest: 'public/assets/styles/sass/foundation', expand: true, flatten: false},
+          {isFile: true, rename: function(dest, src){ return dest + "_" + src; }, cwd: "bower_components/foundation/scss", src: 'foundation.scss', dest: 'public/assets/styles/sass/', expand: true, flatten: false},
+          {isFile: true, rename: function(dest, src){ return dest + "_" + src; }, cwd: "bower_components/foundation/scss", src: 'normalize.scss', dest: 'public/assets/styles/sass/', expand: true, flatten: false},
 
-          {expand: true, flatten: false, cwd: "bower_modules/jquery", src: 'jquery.js', dest: 'public/assets/scripts/vendor/', filter: 'isFile'},
-          {expand: true, flatten: false, cwd: "bower_modules/requirejs", src: 'require.js', dest: 'public/assets/scripts/vendor/', filter: 'isFile'},
-          {expand: true, flatten: false, cwd: "bower_modules/underscore", src: 'underscore.js', dest: 'public/assets/scripts/vendor/', filter: 'isFile'},
+          {expand: true, flatten: false, cwd: "bower_components/jquery", src: 'jquery.js', dest: 'public/assets/scripts/vendor/', filter: 'isFile'},
+          {expand: true, flatten: false, cwd: "bower_components/requirejs", src: 'require.js', dest: 'public/assets/scripts/vendor/', filter: 'isFile'},
+          {expand: true, flatten: false, cwd: "bower_components/underscore", src: 'underscore.js', dest: 'public/assets/scripts/vendor/', filter: 'isFile'},
         ]
       }
     },
