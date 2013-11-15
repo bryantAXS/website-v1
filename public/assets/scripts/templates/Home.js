a77f7d103b6d0866ea2f13d571d52a591431484e
Bryant Hughes <bryant@thegoodlab.com>
2013-11-14 22:05:08 -0600

    adding animations

--- public/assets/scripts/templates/Home.js
+++ public/assets/scripts/templates/Home.js
@@ -5,6 +5,9 @@ define([
 
   var Home = function(){
 
+    this.processClasses = ["is-planning", "is-designing","is-coding", "is-launching"];
+    this.currentProcessClassIndex = 0;
+
   };
 
   Home.prototype.init = function(){
@@ -12,6 +15,22 @@ define([
     var self = this;
 
     $(".hero").backstretch("/assets/images/bg-hero.png");
+
+    var cb = function(){
+      self.changeProcess();
+    };
+
+    setInterval(cb, 5000);
+
+  };
+
+  Home.prototype.changeProcess = function(){
+
+    this.currentProcessClassIndex += 1;
+
+    $(".svg-process-container")
+      .removeClass(this.processClasses.join(" "))
+      .addClass(this.processClasses[this.currentProcessClassIndex]);
 
   };
 
