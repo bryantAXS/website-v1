f86a53badc62d5debfcb77eb0f461c6fba5018f1
Bryant Hughes <bryant@thegoodlab.com>
2013-11-26 17:10:48 -0600

    hooking up gallery navigation

--- public/assets/scripts/classes/WorkGallery.js
+++ public/assets/scripts/classes/WorkGallery.js
@@ -10,20 +10,23 @@ define([
   Cycle2
   ){
 
-  var startingSlides = {
-    dimos: 1,
-    broomfield: 4,
-    plymold: 7,
-  };
 
+  /**
+   * Constructor mo'fucka
+   */
   var WorkGallery = function(){
 
     var self = this;
 
     this.$allNavs = $(".work-slides-navigation");
-
   };
 
+
+
+  /**
+   * Getting things underway
+   * @return {[type]} [description]
+   */
   WorkGallery.prototype.init = function(){
 
     this.gallery = $(".work-slides-container").cycle({
@@ -31,6 +34,7 @@ define([
       timeout: 0
     });
 
+    // setup our bg image
     $(".work-slides-container").backstretch("/assets/images/bg-work.png");
 
     this.initProjectButtons();
@@ -38,45 +42,89 @@ define([
 
   };
 
+
+  /**
+   * Hooking up the buttons on the first slide
+   * @return {[type]} [description]
+   */
   WorkGallery.prototype.initProjectButtons = function(){
 
     var self = this;
 
-    // project buttons
     $(".work-button").on("click", function(){
 
       var projectName = $(this).data("project");
-      var slideIndex = startingSlides[projectName];
+      var projectSlideIndex = 0;
+      var absoluteSlideIndex = self.getAbsoluteSlideIndex(projectName, projectSlideIndex);
 
-      self.gallery.cycle("goto", slideIndex);
-
+      self.gallery.cycle("goto", absoluteSlideIndex);
+      self.setNavigation(projectName, projectSlideIndex);
       $("#"+projectName+"-navigation").transition({top: 0}, 350);
 
     });
 
   };
 
+
+  /**
+   * Hooking up individual navigation items
+   * @return {[type]} [description]
+   */
   WorkGallery.prototype.initNavigations = function(){
 
     var self = this;
 
     $(".work-slides-navigation .arrow-left").on("click", function(){
-
       self.gallery.cycle("goto", 0);
       self.closeNavigation();
-
     });
 
     $(".work-slides-navigation .arrow-right").on("click", function(){
-
-      // TODO: this is where we are leaving off
-
+      self.gotoNextSlide();
     });
 
   };
 
+  WorkGallery.prototype.gotoNextSlide = function(){
+
+    var self = this;
+    var $activeSlide = $(".cycle-slide-active");
+    var project = $activeSlide.data("project");
+    var projectIndex = Number($activeSlide.data("slide"));
+
+    var nextAbsoluteSlideIndex = this.getAbsoluteSlideIndex(project, projectIndex + 1);
+
+    if(nextAbsoluteSlideIndex === undefined){
+      self.gallery.cycle("goto", 0);
+      self.closeNavigation();
+    }else{
+      self.gallery.cycle("goto", nextAbsoluteSlideIndex);
+      self.setNavigation(project, projectIndex + 1);
+    }
+
+  };
+
+  WorkGallery.prototype.setNavigation = function(projectName, projectIndex){
+    $(".work-slides-navigation .project").removeClass("is-active");
+    $(".work-slides-navigation .project[data-project='"+projectName+"'][data-slide='"+projectIndex+"']").addClass("is-active");
+  };
+
+  /**
+   * Close all project specific navigation
+   * @return {[type]} [description]
+   */
   WorkGallery.prototype.closeNavigation = function(){
     this.$allNavs.transition({top: -65}, 350);
+  };
+
+  /**
+   * Return the absolute slide index based on a project's name and project slide index
+   * @param  {[type]} projectName  [description]
+   * @param  {[type]} projectIndex [description]
+   * @return {[type]}              [description]
+   */
+  WorkGallery.prototype.getAbsoluteSlideIndex = function(projectName, projectIndex){
+    return $(".slide[data-project='"+projectName+"'][data-slide='"+projectIndex+"']").data("absolute-slide-index");
   };
 
   return WorkGallery;
